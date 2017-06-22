c434597f8f8cfe2ddbae346f1e6551be9676175c
SpartanSOS <markb539@gmail.com>
2017-06-22 12:16:25 -0500

    auth before maps

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+// 3rd Party Libraries
 import Expo from 'expo';
 import React from 'react';
+import firebase from 'firebase';
+// import { TabNavigator, StackNavigator } from 'react-navigation';
+
+// Relative Imports
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
+
+// Styles
 import { StyleSheet, Text, View } from 'react-native';
 
+
 class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyBEIuNlAAKU8byP2NUptaZTPtHobhYqMQA",
+      authDomain: "hasty-14d18.firebaseapp.com",
+      databaseURL: "https://hasty-14d18.firebaseio.com",
+      projectId: "hasty-14d18",
+      storageBucket: "hasty-14d18.appspot.com",
+      messagingSenderId: "734280961973"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -17,7 +40,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
 
