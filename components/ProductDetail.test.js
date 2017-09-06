8b4c0db755324d2360e98d9c4052440fd67329d3
Ethan Tran <ethan@joinronin.com>
2017-09-06 17:18:25 -0500

    Update ProductDetail test

--- components/ProductDetail.test.js
+++ components/ProductDetail.test.js
@@ -7,11 +7,17 @@ import ProductDetail from './ProductDetail';
 
 const initialState = {};
 
+const product = {
+    title: 'Redbull',
+    thumbnail_image: 'https://facebook.github.io/react/img/logo_og.png',
+    price: '$3.49'
+};
+
 describe('ProductDetail', () => {
     const middlewares = [];
     const mockStore = configureStore(middlewares);
     it('renders correctly', () => {
-        const wrapper = shallow(<ProductDetail />, {
+        const wrapper = shallow(<ProductDetail product={product} />, {
             context: { store: mockStore(initialState) }
         });
         const render = wrapper.dive();
