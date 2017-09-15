48480b5481907539f7108cb6d121d5566bfb8492
Ethan Tran <ethan@joinronin.com>
2017-09-14 19:57:11 -0500

    HAMO-34: Match mockup

---
+++ components/SectionTitle.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+import Color from '../constants/Color';
+import { emY } from '../utils/em';
+
+export default function SectionTitle({ title, style, ...props }) {
+    return (
+        <Text {...props} style={[styles.container, style]}>
+            {title}
+        </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        fontSize: emY(0.8125),
+        color: Color.GREY_600,
+        paddingHorizontal: 15,
+        paddingTop: emY(2.1875),
+        paddingBottom: emY(1)
+    }
+});
