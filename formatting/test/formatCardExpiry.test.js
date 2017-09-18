56039e7348bd5c854be0e29cd0861c0ed946d6d2
Ken <brightnesst99@gmail.com>
2017-09-19 01:21:54 +0900

    updated the formatting function test

--- formatting/test/formatCardExpiry.test.js
+++ formatting/test/formatCardExpiry.test.js
@@ -2,8 +2,8 @@ import formatCardExpiry from '../formatCardExpiry';
 
 describe('formatCardExpiry', () => {
     it('formatCardNumber works correctly', () => {
-        const str = '';
-        const expected = '';
+        const str = '0123456';
+        const expected = '01 / 2345';
         expect(formatCardExpiry(str)).toBe(expected);
     });
 });
