ab779db4fc430a42014fa8016dbe1b1dd1d156dc
Ethan Tran <ethan@joinronin.com>
2017-10-18 10:04:30 -0500

    Hamo 76 (#55)
    * HAMO-76: Change success icon
    
    * Update tests

--- containers/tests/SignUpForm.test.js
+++ containers/tests/SignUpForm.test.js
@@ -4,10 +4,15 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 
-import { SignUpForm } from '../SignUpForm';
+import SignUpForm from '../SignUpForm';
 
 describe('SignUpForm', () => {
+    const middlewares = [];
+    const mockStore = configureStore(middlewares);
     it('should render correctly', () => {
+        const initialState = {
+            auth: {}
+        };
         const props = {
             anyTouched: false,
             pending: false,
@@ -26,12 +31,19 @@ describe('SignUpForm', () => {
                     touched: false,
                     error: null
                 }
-            }
+            },
+            handleSubmit: jest.fn(),
+            actions: {}
         };
-        const render = shallow(<SignUpForm {...props} />);
+        const render = shallow(<SignUpForm {...props} />, {
+            context: { store: mockStore(initialState) }
+        });
         expect(render).toMatchSnapshot();
     });
     it('should render correctly with untouched disabled', () => {
+        const initialState = {
+            auth: {}
+        };
         const props = {
             anyTouched: false,
             pending: false,
@@ -50,12 +62,19 @@ describe('SignUpForm', () => {
                     touched: false,
                     error: null
                 }
-            }
+            },
+            handleSubmit: jest.fn(),
+            actions: {}
         };
-        const render = shallow(<SignUpForm {...props} />);
+        const render = shallow(<SignUpForm {...props} />, {
+            context: { store: mockStore(initialState) }
+        });
         expect(render).toMatchSnapshot();
     });
     it('should render correctly with error', () => {
+        const initialState = {
+            auth: {}
+        };
         const props = {
             anyTouched: true,
             pending: false,
@@ -74,12 +93,19 @@ describe('SignUpForm', () => {
                     touched: false,
                     error: null
                 }
-            }
+            },
+            handleSubmit: jest.fn(),
+            actions: {}
         };
-        const render = shallow(<SignUpForm {...props} />);
+        const render = shallow(<SignUpForm {...props} />, {
+            context: { store: mockStore(initialState) }
+        });
         expect(render).toMatchSnapshot();
     });
     it('should render correctly with submitting', () => {
+        const initialState = {
+            auth: {}
+        };
         const props = {
             anyTouched: true,
             pending: false,
@@ -98,12 +124,19 @@ describe('SignUpForm', () => {
                     touched: false,
                     error: null
                 }
-            }
+            },
+            handleSubmit: jest.fn(),
+            actions: {}
         };
-        const render = shallow(<SignUpForm {...props} />);
+        const render = shallow(<SignUpForm {...props} />, {
+            context: { store: mockStore(initialState) }
+        });
         expect(render).toMatchSnapshot();
     });
     it('should render correctly with submitSuccess', () => {
+        const initialState = {
+            auth: {}
+        };
         const props = {
             anyTouched: true,
             pending: false,
@@ -123,9 +156,13 @@ describe('SignUpForm', () => {
                     touched: false,
                     error: null
                 }
-            }
+            },
+            handleSubmit: jest.fn(),
+            actions: {}
         };
-        const render = shallow(<SignUpForm {...props} />);
+        const render = shallow(<SignUpForm {...props} />, {
+            context: { store: mockStore(initialState) }
+        });
         expect(render).toMatchSnapshot();
     });
 });
