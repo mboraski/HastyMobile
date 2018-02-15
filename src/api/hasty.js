71116c326cb70671028d1b77d2b1236a4136dd6a
Ethan Tran <ethan@joinronin.com>
2018-02-15 15:25:50 -0600

    export hasty api for testing

--- src/api/hasty.js
+++ src/api/hasty.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const instance = axios.create({
+export const instance = axios.create({
     baseURL: 'https://us-central1-hasty-14d18.cloudfunctions.net/'
 });
 // const instance = axios.create({
