d917572d2c3a94cd3a5495e1c0eaac05ae51d84c
Mark Boraski <markb539@gmail.com>
2018-05-13 14:39:45 -0500

    add prettier

--- main.js
+++ main.js
@@ -1,58 +1,60 @@
 // Third Party Imports
-import React, { Component } from 'react';
-import { StyleSheet, Image, Platform, ActivityIndicator } from 'react-native';
-import Expo, { Font } from 'expo';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react';
+import React, { Component } from "react";
+import { StyleSheet, Image, Platform, ActivityIndicator } from "react-native";
+import Expo, { Font } from "expo";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 // Relative Imports
-import splashImage from './src/assets/splash.png';
-import Color from './src/constants/Color';
-import RootContainer from './src/containers/RootContainer';
-import { store, persistor } from './src/store';
+import splashImage from "./src/assets/splash.png";
+import Color from "./src/constants/Color";
+import RootContainer from "./src/containers/RootContainer";
+import { store, persistor } from "./src/store";
 
 class App extends Component {
-    state = {
-        fontLoaded: false
+  state = {
+    fontLoaded: false
+  };
+
+  async componentDidMount() {
+    const fonts = {
+      goodtimes: require("./src/assets/fonts/goodtimes.ttf")
     };
 
-    async componentDidMount() {
-        const fonts = {
-            goodtimes: require('./src/assets/fonts/goodtimes.ttf')
-        };
-        if (Platform.OS === 'android') {
-            fonts.Arial = require('./src/assets/fonts/arial.ttf');
-        }
-        await Font.loadAsync(fonts);
-        this.setState({ fontLoaded: true });
+    if (Platform.OS === "android") {
+      fonts.Arial = require("./src/assets/fonts/arial.ttf");
     }
 
-    render() {
-        return this.state.fontLoaded ? (
-            <Provider store={store}>
-                <PersistGate
-                    persistor={persistor}
-                    loading={
-                        <Image
-                            source={splashImage}
-                            style={[StyleSheet.absoluteFill, styles.splash]}
-                            resizeMode="contain"
-                        />
-                    }
-                >
-                    <RootContainer />
-                </PersistGate>
-            </Provider>
-        ) : (
-            <ActivityIndicator size={'large'} />
-        );
-    }
+    await Font.loadAsync(fonts);
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    return this.state.fontLoaded ? (
+      <Provider store={store}>
+        <PersistGate
+          persistor={persistor}
+          loading={
+            <Image
+              source={splashImage}
+              style={[StyleSheet.absoluteFill, styles.splash]}
+              resizeMode="contain"
+            />
+          }
+        >
+          <RootContainer />
+        </PersistGate>
+      </Provider>
+    ) : (
+      <ActivityIndicator size={"large"} />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    splash: {
-        backgroundColor: Color.YELLOW_600
-    }
+  splash: {
+    backgroundColor: Color.YELLOW_600
+  }
 });
 
 Expo.registerRootComponent(App);
